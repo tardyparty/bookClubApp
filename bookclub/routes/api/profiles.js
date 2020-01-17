ac34efaab1b382da1d2f85669975e9f8f4a6bbb8
tardyparty <tardyparty@users.noreply.github.com>
2020-01-17 02:26:22 -0600

    endpoints for following

--- bookclub/routes/api/profiles.js
+++ bookclub/routes/api/profiles.js
@@ -27,4 +27,30 @@ router.get('/:username', auth.optional, function(req, res, next) {
     }
 });
 
+// follow
+router.post('/:username/follow', auth.required, function(req, res, next) {
+    let profileId = req.profile._id;
+    
+    User.findById(req.payload.id).then(function(user) {
+        if (!user) { return res.sendStatus(401); }
+
+        return user.follow(profileId).then(function() {
+            return res.json({ profile: req.profile.toProfileJSONFor(user)});
+        });
+    }).catch(next);
+});
+
+// unfollow
+router.delete('/:username/follow', auth.required, function(req, res, next) {
+    let profileId = req.profile._id;
+
+    User.findById(req.payload.id).then(function(user) {
+        if (!user) { return res.sendStatus(401); } 
+        
+        return user.unfollow(profileId).then(function() {
+            return res.json({ profile: req.profile.toProfileJSONFor(user)});
+        });
+    }).catch(next);
+});
+
 module.exports = router;
